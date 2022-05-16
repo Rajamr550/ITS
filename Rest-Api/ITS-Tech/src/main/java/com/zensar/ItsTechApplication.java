f72e05d8ed7162c009275ec8157872a66fec6b2c
Rajamr550 <45516738+Rajamr550@users.noreply.github.com>
2022-05-16 20:03:57 +0530

    init-3
    init-3

---
+++ Rest-Api/ITS-Tech/src/main/java/com/zensar/ItsTechApplication.java
@@ -0,0 +1,13 @@
+package com.zensar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsTechApplication.class, args);
+	}
+
+}
