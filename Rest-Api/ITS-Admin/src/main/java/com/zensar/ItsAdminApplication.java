09d4f2d0828d67d53ea46eb671dfd8ba9b78a577
Rajamr550 <45516738+Rajamr550@users.noreply.github.com>
2022-05-16 19:56:46 +0530

    Add files via upload

---
+++ Rest-Api/ITS-Admin/src/main/java/com/zensar/ItsAdminApplication.java
@@ -0,0 +1,13 @@
+package com.zensar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsAdminApplication.class, args);
+	}
+
+}
