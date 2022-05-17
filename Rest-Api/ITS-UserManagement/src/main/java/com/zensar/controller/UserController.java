355b97c3d330b4e093312f7d833cf115dc785a8e
SR66171 <SR66171@POOLW42SLPC6437.ind.zensar.com>
2022-05-17 10:33:14 +0530

    third commit

---
+++ Rest-Api/ITS-UserManagement/src/main/java/com/zensar/controller/UserController.java
@@ -0,0 +1,28 @@
+/*
+ * User Controller
+ * 
+ * 1.http://localhost:8003/admin/login - post
+ * 
+ * 2.http://localhost:8003/tech/login - post
+ * 
+ * 3.http://localhost:8003/hr/login - post
+ * 
+ * 4.http://localhost:8003/admin/validates - GET
+ * 
+ * 5/.http://localhost:8003/tech/validates - GET
+ * 
+ * 6. http://localhost:8003/hr/validates - GET
+
+7.8.9 --- logsout each user [ admin,hr,tech] - Delete
+
+ * 	
+ * 	
+ * 
+ * 
+ */
+
+package com.zensar.controller;
+
+public class UserController {
+
+}
---
+++ Rest-Api/ITS-UserManagement/src/main/java/com/zensar/repo/HrRepo.java
@@ -0,0 +1,5 @@
+package com.zensar.repo;
+
+public class HrRepo {
+
+}
